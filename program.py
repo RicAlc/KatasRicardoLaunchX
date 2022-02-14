76a59c392d1d48c1b829634d7b566f383b6c96b9
RicAlc <alcala.ricardoc@gmail.com>
2022-02-14 02:49:20 -0600

    add: Katas 1-9

---
+++ program.py
@@ -0,0 +1,11 @@
+# program.py
+sum = 5+5
+print(sum)
+
+# Impresion en pantalla print('Hola desde la consola')
+print ('Hola desde la consola')
+
+#VARIABLES
+sum1 = 1 + 2 # 3
+product = sum1 * 2
+print(product)
